fc00973faa4504e117b0e78dcf9065f7f1c1e46a
Mechanic Josh <joshuadunbar@gmail.com>
2024-03-27 14:52:41 -0400

    completed fully functional code

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ let friends = ['Amber', 'Anthony', 'Liv', 'Slone', 'Mark'];
 function songLoop(){
     for (let f of friends){
         friendName = f;
+        let myFriend = document.createElement('div');
+        document.body.appendChild(myFriend);
+        myFriend.className = 'friend';
+        let myFriendH3 = document.createElement('h3');
+        myFriend.appendChild(myFriendH3);
+        myFriendH3.textContent = f;
         console.log(f);{
             for(let i = 99; i > 0; i--){
-                console.log(i + " lines of code in the file, " + i +  " lines of code; " + friendName + " strikes one out, clears it all out, " + (i-1) + " lines of code in the file");
+                let p = document.createElement('p');
+                myFriend.appendChild(p);
+                p.textContent = i + " lines of code in the file, " + i +  " lines of code; " + friendName + " strikes one out, clears it all out, " + (i-1) + " lines of code in the file"
             } 
         }  
     }
